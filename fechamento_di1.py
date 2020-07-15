42745172722f9a2eccc44fff1059c176dbf71512
mtrj <57919946+mtrj@users.noreply.github.com>
2020-07-15 16:41:57 -0300

    Update fechamento_di1.py

--- fechamento_di1.py
+++ fechamento_di1.py
@@ -3,10 +3,12 @@ from datetime import date
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import getpass
 
 class fechamento:
     def __init__(self):
         self.headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+        self.path = f'C:\\Users\\{getpass.getuser()}\\Desktop\\base_DI1\\'
         
     def di1(self, data, export=False):
         url = f'http://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/SistemaPregao_excel1.asp?Data={data}&Mercadoria=DI1&XLS=true'
@@ -50,3 +52,27 @@ class fechamento:
         dt_arq = dt_spt[2]+'-'+dt_spt[1]+'-'+dt_spt[0]
         if export==True: df.to_csv(f'{self.path}DI1 {dt_arq}.csv')
         return df
+
+    def relatorio_bmf(self,arquivo_datas='datas_curva_pre.txt'):
+        self.arquivo_datas = arquivo_datas
+        df=pd.DataFrame()
+        # Importa as datas desejadas
+        with open(self.arquivo_datas, 'r') as f:
+            self.datas = [line.replace('\n','') for line in f.readlines()]
+        f.close()
+        # Importa as datas que dão erro
+        # with open(self.arquivo_erros, 'r') as f:
+        #     self.dt_err = [line.replace('\n','') for line in f.readlines()]
+        # f.close()
+        # Gera as datas que serão utilizadas
+        dt_t=[]
+        for i in trange(len(self.datas),position=0,leave=True):
+            dt_spt = self.datas[i].replace('/','-').split('-')
+            dt_arq = dt_spt[2]+'-'+dt_spt[1]+'-'+dt_spt[0]
+            nome_arquivo = f'{self.path}DI1 {dt_arq}.csv'
+            if os.path.isfile(nome_arquivo)==False:
+                # if self.datas[i] not in self.dt_err:
+                dt_t.append(self.datas[i])
+            elif os.path.isfile(nome_arquivo)==True:
+                df=df.append(pd.read_csv(nome_arquivo, sep=',', index_col=0))
+                
